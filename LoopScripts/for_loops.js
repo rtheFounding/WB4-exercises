073ed86bfb49db8181221a7852d443999fe512e7
Randy Rivera <rivera.na2015@gmail.com>
2022-10-09 23:50:02 -0400

    Loop Basics Exercises

---
+++ LoopScripts/for_loops.js
@@ -0,0 +1,6 @@
+let num = 1;
+
+for (let i = 0; i < 7; i++) {
+    num = num * 2;
+    console.log(+ num + ": I love loops");
+}
---
+++ LoopScripts/while_loops.js
@@ -0,0 +1,8 @@
+let i = 1;
+let num = 1;
+
+while (i < 8) {
+    num = num * 2;
+    console.log(+ num + ": I love loops");
+    i++;
+}
